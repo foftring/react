41c82054014e54797341bbbed7b755b3deb66080
Frank <frank.oftring@protonmail.com>
2024-08-16 11:06:19 -0400

    add assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'expo-router';
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold">Hello World!</Text>
+      <Text className="text-3xl font-pblack">Hello World!</Text>
       <Link href="/profile">
         <Text>Go to Profile</Text>
       </Link>
